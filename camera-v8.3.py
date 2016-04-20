b6d95baf89decf779eeb344535abd7c1d5780b53
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-04-20 20:23:06 +0300

    Update camera-v8.3.py

--- camera-v8.3.py
+++ camera-v8.3.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 
 #########################################################
-#       PYTHON CAMERA v8.3 SOFTWARE- GH0ST S0FTWARE     #
+#       PYTHON CAMERA v8.3 SOFTWARE - GH0ST S0FTWARE    #
 ######################################################### 
 #                       CONTACT                         #
 #########################################################
@@ -23,16 +23,23 @@ icon_txt = """
 \____/_/  |_/_/  /_/_____/_/ |_/_/  |_|   |___/\____(_)____/  
                                                                                                            
 """
+
 print icon_txt
+
 pygame.camera.init()
 kamera = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 kamera.start()
 img = kamera.get_image()
 cm = "***************************************************************"
+
 print cm
+
 img_name = raw_input("Enter a name : ")
 pygame.image.save(img, img_name)
 loading = "Photo saved... ( Loading... %100 )"
+
 print loading
+
 print cm
+
 pygame.camera.quit()
