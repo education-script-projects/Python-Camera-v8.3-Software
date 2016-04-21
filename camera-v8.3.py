ea27fbf8a46627dddd62d613856745dc37b61800
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-04-21 18:23:46 +0300

    Update camera-v8.3.py

--- camera-v8.3.py
+++ camera-v8.3.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-#########################################################
-#       PYTHON CAMERA v8.3 SOFTWARE - GH0ST S0FTWARE    #
-######################################################### 
-#                       CONTACT                         #
-#########################################################
-#              DEVELOPER : İSMAİL TAŞDELEN              #                       
-#           GMAIL : pentestdatabase@gmail.com           #
-# Linkedin : https://www.linkedin.com/in/ismailtasdelen #
-#           Whatsapp : + 90 534 295 94 31               #
-#########################################################
-
 import pygame.camera
 import pygame.image
 
-icon_txt = """
+camerasoftware_ico = """
    _________    __  _____________  ___            ____   _____
   / ____/   |  /  |/  / ____/ __ \/   |    _   __( __ ) |__  /
  / /   / /| | / /|_/ / __/ / /_/ / /| |   | | / / __  |  /_ < 
 / /___/ ___ |/ /  / / /___/ _, _/ ___ |   | |/ / /_/ / ___/ / 
 \____/_/  |_/_/  /_/_____/_/ |_/_/  |_|   |___/\____(_)____/  
-                                                                                                           
+
+ #########################################################
+ #       PYTHON CAMERA v8.3 SOFTWARE - GH0ST S0FTWARE    #
+ ######################################################### 
+ #                       CONTACT                         #
+ #########################################################
+ #              DEVELOPER : İSMAİL TAŞDELEN              #                       
+ #           GMAIL : pentestdatabase@gmail.com           #
+ # Linkedin : https://www.linkedin.com/in/ismailtasdelen #
+ #           Whatsapp : + 90 534 295 94 31               #
+ #########################################################                                                                                                           
 """
 
-print icon_txt
+print camerasoftware_ico
 
 pygame.camera.init()
 kamera = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 kamera.start()
 img = kamera.get_image()
-cm = "***************************************************************"
+star = "***************************************************************"
 
-print cm
+print star
 
-img_name = raw_input("Enter a name : ")
+img_name = raw_input("Fotoğrafınıza bir isim belirleyiniz (example.jpg) : ")
 pygame.image.save(img, img_name)
-loading = "Photo saved... ( Loading... %100 )"
+yükleniyor = "Fotoğrafınız kaydedildi... ( Yükleniyor... %100 )"
 
-print loading
+print yükleniyor
 
-print cm
+print star
 
 pygame.camera.quit()
